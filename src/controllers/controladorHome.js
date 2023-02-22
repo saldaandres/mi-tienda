f5732fd4842cc7c15ad78e7b78aa0608247d442e
saldaandres <saldaandres@hotmail.es>
2023-02-22 08:55:21 -0500

    Se crean los productos

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -1,6 +1,26 @@
-// creacion de variables y scope
-let nombre = "Andres"
-if (5 > 2) {
-    let nombre = "Lucho"
-}
-console.log("Hola " + nombre)
+import {productos} from "../helpers/baseDatos.js"
+
+// llamando a la fila
+let fila = document.getElementById("fila")
+
+// recorriendo el arreglo
+productos.forEach(function (producto) {
+    // creando columnas para alojar los productos
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    // crear una tarjeta para almacenar la info del producto
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card", "h-100", "shadow")
+
+    // crear una imagen
+    let imagen = document.createElement("img")
+    imagen.classList.add("img-fluid", "w-100")
+    imagen.src = producto.fotos[0]
+
+    // padres e hijos
+    tarjeta.appendChild(imagen)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+})
+
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,42 @@
+// quemar una base de datos
+export let productos = [
+    {
+        nombre: "Producto 1",
+        descripcion: "Aqui va la descripcion",
+        fotos: ["https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-colgande-2.jpg?alt=media&token=f3292ba0-f3da-45ca-b5e8-64ee724425f9", "https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-colgande.jpg?alt=media&token=194b2227-c6ca-4b79-b40e-49d244e4229a"],
+        precio: 70000,
+        popularidad: 4.2,
+    },
+    {
+        nombre: "Producto 2",
+        descripcion: "Aqui va la descripcion",
+        fotos: ["https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-cubica-1.webp?alt=media&token=e2f04e64-52b7-4e4a-8970-32db613402a4", "https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-cubica-2.webp?alt=media&token=72e48dad-7569-4dcd-9393-3b65205b9106"],
+        precio: 70000,
+        popularidad: 4.2,
+    },
+    {
+        nombre: "Producto 3",
+        descripcion: "Aqui va la descripcion",
+        fotos: ["https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-flotante-1.jpg?alt=media&token=c1ee6d7b-a555-4f69-8899-0ada52121c1e", "https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-flotante-2.jpg?alt=media&token=e0eedbc6-cdcf-4070-be0c-c2668044b949"],
+        precio: 70000,
+        popularidad: 4.2,
+    },
+    {
+        nombre: "Producto 4",
+        descripcion: "Aqui va la descripcion",
+        fotos: ["https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcomedero-altura-1.jpg?alt=media&token=a2024245-6d9b-497a-b31e-8dfed2cfb0fa", "https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcomedero-altura-2.jpg?alt=media&token=d6c1cd5c-e957-4187-a0b7-135ba3d47310"],
+        precio: 70000,
+        popularidad: 4.2,
+    },
+    {
+        nombre: "Producto 5",
+        descripcion: "Aqui va la descripcion",
+        fotos: ["https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-rascador-1.webp?alt=media&token=f8592e54-38b4-4ab5-9f8d-3be352ceb693", "https://firebasestorage.googleapis.com/v0/b/proyecto-tienda-andres.appspot.com/o/imagenes-productos%2Fcama-rascador-2.webp?alt=media&token=1cbb6f3f-ad06-4b67-884e-74e6174a844e"],
+        precio: 70000,
+        popularidad: 4.2,
+    },
+];
+
+
+
+
