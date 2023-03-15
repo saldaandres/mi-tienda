fec3c7a67ed20fe824d51c1c4309054ee10c9e57
saldaandres <saldaandres@hotmail.es>
2023-03-15 07:32:05 -0500

    Configurar un objeto de localStrage para almacenar el carrito de compras

--- src/controllers/controladorCompra.js
+++ src/controllers/controladorCompra.js
@@ -12,6 +12,23 @@ let precio = document.getElementById("precio")
 precio.innerHTML = formateador.format(infoProducto.precio)
 let descripcion = document.getElementById("descripcion")
 descripcion.textContent = infoProducto.descripcion
+let spanCantidad = document.getElementById("spanCarrito")
+
+let infoCarrito = JSON.parse(localStorage.getItem("carrito"))
+let carrito = []
+if (infoCarrito != null) {
+    carrito = infoCarrito
+    spanCantidad.textContent = carrito.length
+}
+// carrito de compras
+let cantidad = document.getElementById("cantidad")
+let botonAgregar = document.getElementById("agregarAlCarrito")
+botonAgregar.addEventListener("click", () => {
+    carrito.push(infoProducto)
+    spanCantidad.textContent = carrito.length
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+})
+
 
 
 // convertiendo las monedas
@@ -20,7 +37,6 @@ let botonConvertir = document.getElementById("conversor")
 botonConvertir.addEventListener("click", async () => await ejecutarApiTasas())
 
 // evitando problemas con el input de cantidad
-let cantidad = document.getElementById("cantidad")
 cantidad.addEventListener("keydown", (tecla) => {
     let teclasProhibidas = ["e", "-", "+", "*", "/"]
     if (teclasProhibidas.includes(tecla.key)) {
